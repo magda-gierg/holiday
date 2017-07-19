5b1689467f95f060d3fc582880f691640a2cb90b
Your Name <you@example.com>
2017-07-20 11:36:47 +1200

    remove route and db connection for heroku hello world

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ var router = express.Router()
 var db = require('../db')
 
 router.get('/', function (req, res) {
-  db.getWeather(req.app.get('connection'))
-    .then(function (weather) {
-      res.render('index', { weather: weather })
-    })
-    .catch(function (err) {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
+  // db.getWeather(req.app.get('connection'))
+  //   .then(function (weather) {
+  //     res.render('index', { weather: weather })
+  //   })
+  //   .catch(function (err) {
+  //     res.status(500).send('DATABASE ERROR: ' + err.message)
+  //   })
+  res.send('hello')
 })
 
 module.exports = router
