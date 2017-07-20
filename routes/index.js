76327ea4a514bb248e993c16632145ebc08ef687
Your Name <you@example.com>
2017-07-20 15:25:19 +1200

    add location

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router()
 
 var db = require('../db')
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
+  res.redirect('/weather')
+})
+
+router.get('/weather', function (req, res) {
   db.getWeather(req.app.get('connection'))
     .then(function (weather) {
       res.render('index', { weather: weather })
@@ -14,15 +18,26 @@ router.get('/', function (req, res) {
   // res.send('hello')
 })
 
-router.get('/:id', function (req, res) {
-  db.getLocation(req.params.id, req.app.get('connection')))
+router.get('/weather/:id', function (req, res) {
+  db.getLocation(req.params.id, req.app.get('connection'))
   .then(function (weather) {
     res.render('location', weather[0])
   })
   .catch(function (err) {
     res.status(500).send('DATABASE ERROR: ' + err.message)
   })
-  // res.send('hello')
 })
+
+router.get('/locations/:id', function (req, res) {
+  db.getActivity(req.params.id, req.app.get('connection'))
+  .then(function (activity) {
+    res.render('activities', activity[0])
+  })
+  .catch(function (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+})
+
+
 
 module.exports = router
--- seeds/weather.js
+++ seeds/weather.js
@@ -2,9 +2,9 @@ exports.seed = function (knex, Promise) {
   return knex('weather').del()
     .then(function () {
       return Promise.all([
-        knex('weather').insert({id: 1, name: 'Sunny', location_id: 1}),
-        knex('weather').insert({id: 2, name: 'wind', location_id: 2}),
-        knex('weather').insert({id: 3, name: 'Snow', location_id: 3})
+        knex('weather').insert({id: 1, name: 'sunny', location_id: 1}),
+        knex('weather').insert({id: 2, name: 'windy', location_id: 2}),
+        knex('weather').insert({id: 3, name: 'snow', location_id: 3})
       ]);
     });
 };
