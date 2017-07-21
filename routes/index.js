b50cee00dc8e241a782ff00b1da07a11d58893ab
Your Name <you@example.com>
2017-07-21 12:39:51 +1200

    lunch

--- routes/index.js
+++ routes/index.js
@@ -19,12 +19,12 @@ router.get('/weather', function (req, res) {
 })
 
 
-router.get('/locations/newactivity/:id', function (req, res) {
-res.render('form', {id: req.params.id})
+router.get('/locations/newactivity/:location_id', function (req, res) {
+res.render('form', {id: req.params.location_id})
 })
 
-router.post('/locations/newactivity/:id', function(req, res) {
-  var id = req.params.id
+router.post('/locations/newactivity/:location_id', function(req, res) {
+  var id = req.params.location_id
   db.addActivity(id, req.body, req.app.get('connection'))
     .then(function () {
       res.redirect('/locations/' + id)
@@ -35,8 +35,8 @@ router.post('/locations/newactivity/:id', function(req, res) {
 })
 
 
-router.get('/weather/:id', function (req, res) {
-  db.getLocations(req.params.id, req.app.get('connection'))
+router.get('/weather/:weather_id', function (req, res) {
+  db.getLocations(req.params.weather_id, req.app.get('connection'))
   .then(function (locations) {
     res.render('location', {locations: locations})
   })
@@ -45,13 +45,35 @@ router.get('/weather/:id', function (req, res) {
   })
 })
 
-router.get('/locations/:id', function (req, res) {
-  db.getActivities(req.params.id, req.app.get('connection'))
+router.get('/locations/:location_id', function (req, res) {
+  db.getActivities(req.params.location_id, req.app.get('connection'))
   .then(function (activities) {
-    // console.log(activities)
-    res.render('activities', {id: req.params.id, name: activities[0].name, activities: activities})
+    res.render('activities', {id: req.params.location_id, name: activities[0].name, activities: activities})
   })
   .catch(function (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+})
+
+router.get('/activities/:activity_id', function (req, res) {
+  db.getPlaces(req.params.activity_id, req.app.get('connection'))
+  .then(function (locations) {
+    console.log({locations});
+    res.render('places', {id: req.params.id, activity_name: locations[0] ? locations[0].activity_name : 'name', locations: locations})
+  })
+  .catch(function (err) {
+    console.log({err});
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+})
+
+router.get('/activities/:activity_id', function (req, res) {
+  db.locationByActivity(req.params.activity_id, req.app.get('connection'))
+  .then(function(loctivities) {
+    res.render('places', {id: req.params.id, location_name: loctivities[0] ? loctivities[0].activity_name : 'name', loctivities: loctivities})
+  })
+  .catch(function (err) {
+    console.log({err});
     res.status(500).send('DATABASE ERROR: ' + err.message)
   })
 })
